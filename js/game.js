a3777a6fdb90277e35f4a35716cfd5bd8ac8cc87
Mike Smith <smithmd@interlochen.org>
2020-02-18 17:04:13 -0500

    map making

--- js/game.js
+++ js/game.js
@@ -8,10 +8,38 @@ let Game = {
     display: null,
 
     _generateAgoraphobiaMap: () => {
-        State.map = new ROT.Map.Cellular(State.w,State.h);
-        State.map.randomize(0.6);
+        const cellular = new ROT.Map.Cellular(State.w,State.h);
+        cellular.randomize(0.6);
         for(let i = 0; i < 3; i++) {
-            State.map.create(this.display.DEBUG);
+            cellular.create();
+        }
+        // fix walls
+        for (let i = 0; i < State.w; i++) {
+            for (let j = 0; j < State.h; j++) {
+                if (i === 0 || i === (State.w-1)) {
+                    cellular._map[i][j] = 0;
+                }
+                if (j === 0 || j === (State.h-1)) {
+                    cellular._map[i][j] = 0;
+                }
+            }
+        }
+
+        const digCallback = function(x, y, value) {
+            if (!value) { return; } /* do not store walls */
+
+            const key = x+","+y;
+            State.map[key] = ".";
+        };
+        cellular.create(digCallback.bind(this));
+    },
+
+    _drawWholeMap: () => {
+        for (var key in State.map) {
+            let parts = key.split(",");
+            let x = parseInt(parts[0]);
+            let y = parseInt(parts[1]);
+            this.display.draw(x, y, State.map[key]);
         }
     },
 
@@ -19,14 +47,7 @@ let Game = {
         this.display = new ROT.Display({width:State.w, height: State.h});
         document.body.appendChild(this.display.getContainer());
         Game._generateAgoraphobiaMap();
-    },
-
-    _drawWholeMap: () => {
-        for (var key in this.map) {
-            let parts = key.split(",");
-            let x = parseInt(parts[0]);
-            let y = parseInt(parts[1]);
-            this.display.draw(x, y, this.map[key]);
-        }
+        Game._drawWholeMap();
+        console.dir(State.map);
     }
 };
